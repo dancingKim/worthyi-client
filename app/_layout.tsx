77142b32555d2a63ef82fca7919d3e42976fc8b5
rightlightfg <rightlightfg@gmail.com>
2025-01-03 07:01:13 +0900

    도로 Slot

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,7 +4,7 @@ import * as SplashScreen from 'expo-splash-screen';
 import React, { useEffect } from 'react';
 import { useColorScheme } from '@/hooks/useColorScheme';
 import { AuthProvider } from "@/context/AuthContext";
-import { Stack } from 'expo-router';
+import { Slot } from 'expo-router';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
 
@@ -53,7 +53,7 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={styles.container}>
       <AuthProvider>
-        <Stack screenOptions={{headerShown: false}}/>
+        <Slot />
       </AuthProvider>
     </GestureHandlerRootView>
   );
