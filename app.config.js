7cd02f9d7c31b994daff21b31b399ac7c11fefec
rightlightfg <rightlightfg@gmail.com>
2025-01-03 15:47:07 +0900

    하나님 제발444

--- app.config.js
+++ app.config.js
@@ -1,51 +1,51 @@
 const getLocalIpAddress = () => {
-  const os = require('os');
-  const networkInterfaces = os.networkInterfaces();
-  
-  for (const interfaceName in networkInterfaces) {
-    const interfaces = networkInterfaces[interfaceName];
-    for (const iface of interfaces) {
-      if (iface.family === 'IPv4' && !iface.internal) {
-        return iface.address;
+    const os = require('os');
+    const networkInterfaces = os.networkInterfaces();
+    
+    for (const interfaceName in networkInterfaces) {
+      const interfaces = networkInterfaces[interfaceName];
+      for (const iface of interfaces) {
+        if (iface.family === 'IPv4' && !iface.internal) {
+          return iface.address;
+        }
       }
     }
-  }
-  return 'localhost';
-};
-
-export default ({ config }) => {
-    const localIpAddress = getLocalIpAddress();
-    const ENV = process.env.NODE_ENV || "development";
-    
-    const envConfig = {
-        development: {
-            OAUTH_BASE_URL: `http://${localIpAddress}.nip.io:8080`,
-            BASE_URL: `http://${localIpAddress}:8080`,
-        },
-        preview: {
-            OAUTH_BASE_URL: "https://api-dev.worthyilife.com",
-            BASE_URL: "https://api-dev.worthyilife.com",
-        },
-        production: {
-            OAUTH_BASE_URL: "https://api-dev.worthyilife.com",
-            BASE_URL: "https://api-dev.worthyilife.com",
-        },
-    };
-
-    const envSettings = envConfig[ENV] || envConfig.development;
-
-    return {
-        ...config,
-        extra: {
-            eas: {
-                projectId: "ba041c45-0229-437a-8d3e-81aba979dd5b"
-            },
-            ENV,
-            localIpAddress,
-            ...envSettings,
-        },
-        updates: {
-            url: "https://u.expo.dev/ba041c45-0229-437a-8d3e-81aba979dd5b"
-        }
-    };
-};
+    return 'localhost';
+  };
+  
+  export default ({ config }) => {
+      const localIpAddress = getLocalIpAddress();
+      const ENV = process.env.APP_ENV || "development";
+      
+      const envConfig = {
+          development: {
+              OAUTH_BASE_URL: `http://${localIpAddress}.nip.io:8080`,
+              BASE_URL: `http://${localIpAddress}:8080`,
+          },
+          preview: {
+              OAUTH_BASE_URL: "https://api-dev.worthyilife.com",
+              BASE_URL: "https://api-dev.worthyilife.com",
+          },
+          production: {
+              OAUTH_BASE_URL: "https://api-dev.worthyilife.com",
+              BASE_URL: "https://api-dev.worthyilife.com",
+          },
+      };
+  
+      const envSettings = envConfig[ENV] || envConfig.development;
+  
+      return {
+          ...config,
+          extra: {
+              eas: {
+                  projectId: "ba041c45-0229-437a-8d3e-81aba979dd5b"
+              },
+              ENV,
+              localIpAddress,
+              ...envSettings,
+          },
+          updates: {
+              url: "https://u.expo.dev/ba041c45-0229-437a-8d3e-81aba979dd5b"
+          }
+      };
+  };
