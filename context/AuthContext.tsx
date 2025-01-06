016c86ce87c4b28fef11742e779e5820ab38eebf
rightlightfg <rightlightfg@gmail.com>
2025-01-06 13:11:43 +0900

    [Fix] 로그인 시 token 저장 안 되는 문제 해결

--- context/AuthContext.tsx
+++ context/AuthContext.tsx
@@ -1,6 +1,6 @@
 import React, { createContext, ReactNode, useContext, useState } from 'react';
 import { User, AuthContextType } from '@/types/types';
-
+import { saveToken } from '@/utils/authStorage';
 
 interface AuthProviderProps {
   children: ReactNode;
@@ -12,6 +12,7 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
   const [user, setUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState(false);
   const login = async (token: string): Promise<void>=> {
+    saveToken(token);
     setIsLoggedIn(true);
     setUser(user);
   };
