ef624cf9c57c057ebb6d8ac1536725db6b8d81e1
rightlightfg <rightlightfg@gmail.com>
2025-02-11 17:28:14 +0900

    profile top 공간 확보

--- app/(app)/(tabs)/profile.tsx
+++ app/(app)/(tabs)/profile.tsx
@@ -162,7 +162,7 @@ const styles = StyleSheet.create({
     },
     header: {
         alignItems: 'center',
-        marginTop: 24,
+        marginTop: 60,
         marginBottom: 32,
     },
     profileImageContainer: {
